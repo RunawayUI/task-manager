7c81a218dd8477ebb269b4977e8f3d413f0ff7a5
RunawayUI <tosha.grafkof@yandex.ru>
2026-04-15 19:24:06 +0300

    added array processing

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function convertDate(date) {
     }).replace(',', '')
 }
 
-const tasks = [
+let tasks = [
     {
         title: "Сделать практику",
         description: "контекст this",
         isCompleted: false,
-        createdDate: convertDate(new Date()),
+        createdDate: new Date(),
         completedDate: null
     }
 ];
@@ -21,7 +21,7 @@ const task = {
     title: "Купить продукты",
     description: "Молоко, хлеб, яйца",
     isCompleted: false,
-    createdDate: convertDate(new Date()),
+    createdDate: new Date(),
     completedDate: null
 };
 let completedTaskCount = 0;
@@ -33,7 +33,7 @@ const setTask = () => {
         title: taskTitle,
         description: taskDescription,
         isCompleted: false,
-        createdDate: convertDate(new Date()),
+        createdDate: new Date(),
         completedDate: null
     }
     return tasks.push(task);
@@ -47,8 +47,8 @@ function showTasks() {
             `Задача: ${title},
             Описание: ${description},
             Статус: ${isCompleted},
-            Создано: ${createdDate}
-            ${completedDate ? `Выполнено: ${completedDate}` : ''}
+            Создано: ${convertDate(createdDate)}
+            ${completedDate ? `Выполнено: ${convertDate(completedDate)}` : ''}
             \n`)
     });
 }
@@ -58,14 +58,26 @@ const completeTask = (index) => {
     if (!tasks[index]) {
         return console.log('Задача отсутствует');
     }
-    const task = tasks[index];
+    // const task = tasks[index];
 
-    task.isCompleted = true;
-    task.completedDate = convertDate(new Date());
+    // task.isCompleted = true;
+    // task.completedDate = convertDate(new Date());
+    // completedTaskCount += 1;
+
+    // console.log(`Задача ${task.title} успешно завершена`);
+
+    tasks = tasks.map((task, i) => {
+        if (i === index) {
+            return {
+                ...task,
+                isCompleted: true,
+                completedDate: convertDate(new Date()),
+            }
+        }
+        return task;
+    });
     completedTaskCount += 1;
-
-    console.log(`Задача ${task.title} успешно завершена`);
-
+    console.log(`Задача ${tasks[index].title} успешно завершена`);
 }
 completeTask(1);
 showTasks();
@@ -95,3 +107,42 @@ const clearTasks = () => {
 }
 clearTasks();
 console.log(tasks);
+
+
+function getTaskDescriptions() {
+    return tasks.map(task => task.description);
+}
+console.log('task descriptions: ', getTaskDescriptions());
+
+
+const startDate = new Date('2026-04-14');
+const endDate = new Date('2026-04-16');
+
+function getTasksByDateRange(startDate, endDate, isCompleted = false) {
+    let dateRangeArray = [];
+    if (isCompleted === true) {
+        console.log('isCompleted = true');
+        dateRangeArray = tasks.filter(task => task.isCompleted === true).filter(task => (task.createdDate > startDate && task.createdDate < endDate) || (task.completedDate > startDate && task.completedDate < endDate))
+    } else {
+        dateRangeArray = tasks.filter(task => task.createdDate > startDate && task.createdDate < endDate)
+    }
+
+    if (dateRangeArray.length > 0) { return dateRangeArray } else { return 'Нет подходящих задач' }
+}
+console.log('tasks by date range: ', getTasksByDateRange(startDate, endDate, true));
+
+function getLongTasks() {
+    return tasks.filter(task => (task.title.length > 10 || task.description.length > 10));
+}
+console.log('long tasks: ', getLongTasks());
+
+function clearShortTasks() {
+    return tasks = tasks.filter(task => task.title.length >= 5);
+}
+console.log(clearShortTasks());
+
+function rename(index, newTitle) {
+    tasks[index].title = newTitle;
+    return tasks;
+}
+console.log(rename(1, 'сделать тренировку на бицепс'));
